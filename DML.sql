c5f8061c5cb639168107d36c6dc81da155c897a9
Tim <9349164+T1mL3arn@users.noreply.github.com>
2019-05-01 14:47:24 +1000

    some date for tests

---
+++ DML.sql
@@ -0,0 +1,21 @@
+-- DML 19 task tests
+insert into Battles values 
+('B1', 41) -- B1
+,('B2', 42) -- B2
+,('B3', 43) -- B3
+,('B4', 44) -- B4
+,('B5', 45) -- B5
+;
+
+insert into Outcomes (ship, battle, result) 
+values
+('A', 'B1','ok')
+,('A', 'B2','damaged')
+,('B', 'B3','damaged')
+,('B', 'B4','sunk')
+,('C', 'B3','damaged')
+,('D', 'B5','damaged')
+,('E', 'B4','ok')
+;
+--
+
